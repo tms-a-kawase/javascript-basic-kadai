7b0e98e1a2cc1c081aca45b6022a334254c25a3e
tms-a-kawase <a-kawase@tms-e.co.jp>
2024-07-13 22:52:10 +0900

    lesson4

---
+++ js/variable.js
@@ -0,0 +1,16 @@
+let userName;
+
+userName = '侍太郎';
+
+let userNumber = 55;
+
+console.log(userName);
+console.log(userNumber);
+
+userName = '侍花子';
+userNumber = 88;
+
+console.log(userName);
+console.log(userNumber);
+
+
