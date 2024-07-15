67bbaea6e81d9dcf6ca588d9063b865f22d32c6f
tms-a-kawase <a-kawase@tms-e.co.jp>
2024-07-15 12:27:39 +0900

    lesson7

---
+++ js/if-switch.js
@@ -0,0 +1,17 @@
+console.log(45 + 18);
+console.log(45 > 18);
+
+console.log('5' == 5);
+console.log('5' === 5);
+
+let num = Math.floor(Math.random() * 5);
+
+console.log(num);
+
+if (num === 4) {
+    console.log('大当たりです');
+} else if (num === 3) {
+    console.log('当たりです');
+} else {
+    console.log('はずれです');
+}
