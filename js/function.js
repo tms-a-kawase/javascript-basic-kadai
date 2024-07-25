ebfed245fe2509e52f547110f65d7dd33acc1f42
tms-a-kawase <a-kawase@tms-e.co.jp>
2024-07-25 10:42:36 +0900

    kadai_mdn

---
+++ js/function.js
@@ -0,0 +1,14 @@
+const sayGoodMorning = () => {
+    console.log('おはようごうざいます！');
+    console.log('昨日はよく眠れましたか？');
+    console.log('今日も一日がんばりましょう！');
+}
+
+const sayGoodEvening = () => {
+    console.log('こんばんは！');
+    console.log('今日も一日お疲れ様でした！');
+}
+
+sayGoodMorning();
+
+sayGoodEvening();
