b6628c4dc808a403bfdb6d1e9d466b51c453184d
tms-a-kawase <a-kawase@tms-e.co.jp>
2024-08-05 11:34:27 +0900

    lesson19

--- js/event.js
+++ js/event.js
@@ -10,5 +10,30 @@ const parentList = document.getElementById('parent-list');
 addBtn.addEventListener('click', () => {
     const childList = document.createElement('li');
     childList.textContent = 'これはリスト要素です';
-    parentList.appendChild(li);
+    parentList.appendChild(childList);
+});
+
+const countBtn = document.getElementById('count-btn');
+
+countBtn.addEventListener('click', () => {
+    const text = document.forms.textForm.textBox.value;
+    console.log(text.length + '文字');
+});
+
+const areaBtn = document.getElementById('area-btn');
+
+areaBtn.addEventListener('click', () => {
+    const area = document.forms.areaForm.area.value;
+    console.log(area);
+});
+
+const osBtn = document.getElementById('os-btn')
+
+osBtn.addEventListener('click', () => {
+    const items = document.forms.osForm.os;
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].checked) {
+            console.log(items[i].value);
+        }
+    }
 });
