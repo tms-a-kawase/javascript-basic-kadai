9ace3f1addff29c24696ff2b715cc3927d671220
tms-a-kawase <a-kawase@tms-e.co.jp>
2024-09-02 11:19:04 +0900

    Creat Feature create random text

--- typing/main.js
+++ typing/main.js
@@ -1,9 +1,38 @@
-const createText =()=>{};
+let untyped = '';
 
-const keyPress = (e) => {};
+const untypedField = document.getElementById('untyped');
 
-const rankCheck = (score) => {};
+const textLists = [
+    'Hello World', 'This is my App', 'How are you?',
+    'Today is sunny', 'I love JavaScript!', 'Good morning',
+    'I am Japanese', 'Let it be', 'Samurai',
+    'Typing Game', 'Information Technology',
+    'I want to be a programmer', 'What day is today?',
+    'I want to build a web app', 'Nice to meet you',
+    'Chrome Firefox Edge Safari', 'machine learning',
+    'Brendan Eich', 'John Resig', 'React Vue Angular',
+    'Netscape Communications', 'undefined null NaN',
+    'Thank you very much', 'Google Apple Facebook Amazon',
+    'ECMAScript', 'console.log', 'for while if switch',
+    'var let const', 'Windows Mac Linux iOS Android',
+    'programming'
+];
 
-const gameOver = (id) => {};
+const createText = () => {
+    let random = Math.floor(Math.random() * textLists.length);
+    console.log(random);
+    untyped = textLists[random];
+    untypedField.textContent = untyped;
+};
+createText();
+const keyPress = (e) => {
+};
 
-const timer = () => {};
+const rankCheck = (score) => {
+};
+
+const gameOver = (id) => {
+};
+
+const timer = () => {
+};
